0b85167d19d3b4d716745b4b7845b9e68a4423eb
Mendelt Siebenga <msiebenga@gmail.com>
2015-03-08 15:38:44 +0100

    Added example 2.4

---
+++ 2.4_mutableinmutable.scala
@@ -0,0 +1,11 @@
+object Main {
+  def main(args: Array[String]) = {
+    val hello = "Hello "
+    var world = "wolrd"
+
+    hello = "hi "
+    world = "world"
+
+    println(hello + world)
+  }
+}
